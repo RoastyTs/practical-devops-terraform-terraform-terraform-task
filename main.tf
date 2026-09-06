3bd7ec88a56b95c204e20b0909a7b2172e070355
RoastyTs <113179558+RoastyTs@users.noreply.github.com>
2026-09-07 02:32:40 +0400

    Add Terraform configuration for Docker containers

--- main.tf
+++ main.tf
@@ -1,1 +1,53 @@
-# main.tf
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+variable "db_root_password" {
+  type = string
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_image" "mariadb" {
+  name         = "mariadb:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "nginx_container"
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+
+  upload {
+    content = "My First and Lastname: Rostislav Tsekhmeister\n"
+    file    = "/usr/share/nginx/html/index.html"
+  }
+}
+
+resource "docker_container" "mariadb" {
+  image = docker_image.mariadb.image_id
+  name  = "mariadb_container"
+
+  ports {
+    internal = 3306
+    external = 3306
+  }
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${var.db_root_password}"
+  ]
+}
